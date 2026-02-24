bc3e4a19c69a725ca43e10f93d5c91473eb0ed69
RyadRachem <nm_rachem@esi.dz>
2026-02-24 09:24:37 +0100

    test file

--- src/tools/__init__.py
+++
@@ -1,7 +0,0 @@
-from .file_tools import read_file, write_file, list_files
-from .analysis_tools import run_pylint, run_pytest
-from .auditor_tools import get_code_for_audit
-from .fixer_tools import apply_refactoring
-from .judge_tools import validate_code
-
-__all__ = ["read_file", "write_file", "list_files", "run_pylint", "run_pytest", "get_code_for_audit", "apply_refactoring", "validate_code"]
--- src/tools/fixer_tools.py
+++
@@ -1,4 +0,0 @@
-from .file_tools import write_file
-
-def apply_refactoring(file_path: str, content: str) -> str:
-    return write_file(file_path, content)
